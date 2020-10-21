addd2b453b4212bb8a3b0b2d4259c51455d9f16f
jubaerhossainece <jubaerkuet@gmail.com>
2020-10-21 11:42:25 +0600

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,89 @@
+//ham burger menu icon toggle
+function myFunction(x) {
+  x.classList.toggle("change");
+  const navMenu = document.querySelector('.options');
+  $(".nav-menu ul").slideToggle(500);
+  $(".nav-menu ul ul").css("display", "none");
+}
+
+$(function() {
+  $('.options > ul > li').hover(function(){
+    $('ul > ul').slideUp();
+    $(this).children('ul.submenu').stop().slideToggle(500);
+  })
+})
+
+// $('ul li').click(function(){
+//   $('ul > ul').slideUp();
+//   $(this).find('ul').slideToggle();
+// })
+
+
+
+//hero image slider
+$('.carousel').carousel({
+  interval: 6000,
+  pause: false
+});
+
+
+//clients carousel
+$(document).ready(function(){
+  $('.content-client').owlCarousel({
+    rtl: true,
+    loop: true,
+    margin: 10,
+    nav: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 3000,
+    lazyLoad: true,
+    navText: ['<i class="arrow left"></i>', '<i class="arrow right"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        576:{
+            items: 2
+        },
+        768:{
+          items: 4
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+});
+
+
+//back to top button
+$(window).scroll(function(){
+  if($(this).scrollTop() > 100) {
+    $('#topScrollBtn').fadeIn('slow');
+  } else{
+    $('#topScrollBtn').fadeOut('slow');
+  }
+});
+
+$('#topScrollBtn').click(function(){
+  $('html, body').animate({
+    scrollTop: 0
+  }, 1000, 'swing');
+  return false;
+});
+
+
+// The syntax:- $fn.scrollspeed(step, speed, easing) 
+// jQuery.scrollspeed(100,800); 
+
+// AOS initialisation
+function aos_init() {
+	AOS.init({
+		duration: 1000,
+		once: true
+	});
+}
+$(window).on('load', function(){
+	aos_init();
+})(jQuery);
